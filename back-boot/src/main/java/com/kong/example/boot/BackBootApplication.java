1710ad9a160275835deff5af06ff9a0f2bfdb449
kongweiteng <kongweiteng@enn.cn>
2019-05-07 10:21:52 +0800

    项目初始化

---
+++ back-boot/src/main/java/com/kong/example/boot/BackBootApplication.java
@@ -0,0 +1,13 @@
+package com.kong.example.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackBootApplication.class, args);
+    }
+
+}
