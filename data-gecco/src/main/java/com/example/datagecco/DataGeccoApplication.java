08dacafbf40a3c90feb7bf3c488c64ffa2cfac94
kongweiteng <kongweiteng@enn.cn>
2019-05-17 16:14:00 +0800

    增加一个需要触发的爬虫

--- data-gecco/src/main/java/com/example/datagecco/DataGeccoApplication.java
+++ data-gecco/src/main/java/com/example/datagecco/DataGeccoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DataGeccoApplication {
 
-//    @Bean
+    //    @Bean
     public SpringGeccoEngine initGecco() {
         return new SpringGeccoEngine() {
             @Override
