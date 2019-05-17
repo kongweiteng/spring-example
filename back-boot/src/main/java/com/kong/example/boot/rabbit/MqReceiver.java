01c3f4a93e9872d3f461c626cc8459abb41a31fd
kongweiteng <kongweiteng@enn.cn>
2019-05-17 11:25:35 +0800

    消费rabbitmq的问题

--- back-boot/src/main/java/com/kong/example/boot/rabbit/MqReceiver.java
+++ back-boot/src/main/java/com/kong/example/boot/rabbit/MqReceiver.java
@@ -14,6 +14,9 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class MqReceiver {
+    //消费数据之前，先
+
+
     //1. 手动创建，需在RabbitMQ中手动创建myQueue1 队列，否则报错
 //    @RabbitListener(queues = "queue.out1")
     public void process1(String message) {
@@ -29,15 +32,26 @@ public class MqReceiver {
 
     /**
      * 监听时自动创建 交换机 、队列 ，并且根据key将队列和交换机绑定
+     * durable 表示是否持久化
      *
      * @param message
      */
     @RabbitListener(bindings = @QueueBinding(
-            exchange = @Exchange("time-exchange"),
-
-            value = @Queue("queue.out1")
+            exchange = @Exchange(value = "time-exchange", durable = "true", type = "fanout"),
+            value = @Queue(value = "time-exchange-data", durable = "true")
     ))
-    public void processComputer(String message) {
-        //log.info("computer MqReceiver: {}", message);
+    public void time(String message) {
+//        log.info("time-exchange-data: {}", message);
+        //消费fanout交换机的时候不需要设置key
+    }
+
+
+    @RabbitListener(bindings = @QueueBinding(
+            exchange = @Exchange(value = "demo", durable = "true", type = "fanout"),
+            value = @Queue(value = "demo-data", durable = "true")
+    ))
+    public void demo(String message) {
+//        log.info("time-exchange-data: {}", message);
+        //消费fanout交换机的时候不需要设置key
     }
 }
